f5ce770cb738bfb2632267d04348acbaf41bdbf0
Salva Gutiérrez <sgutierr@florida-uni.es>
2021-06-20 19:53:04 +0200

    Agregamos la clase Transporte

---
+++ AP01/EJ02/Transporte.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ02
+{
+    class Transporte
+    {
+        Programamos la clase Transporte
+    }
+}
